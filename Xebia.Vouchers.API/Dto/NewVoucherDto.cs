92b0dd823d1d16efbec33fc74e66891fc3bf70f0
Niels Abels <niels.abels@gmail.com>
2020-03-02 12:37:41 +0100

    convert domain to API Dto object

--- Xebia.Vouchers.API/Dto/NewVoucherDto.cs
+++ Xebia.Vouchers.API/Dto/NewVoucherDto.cs
@@ -1,7 +1,23 @@
+using System;
+using System.Configuration;
+using Xebia.Vouchers.Domain;
+
 namespace Xebia.Vouchers.API.Dto
 {
     public class NewVoucherDto
     {
-        
+        public Guid Id { get; set; }
+        public VoucherTypeEnum VoucherType { get; set; }
+
+        public static NewVoucherDto FromDomain(NewVoucher newVoucher)
+        {
+            var dto = new NewVoucherDto()
+            {
+                Id = newVoucher.Id,
+                VoucherType = (VoucherTypeEnum) ((int) newVoucher.VoucherType)
+            };
+            
+            return dto;
+        }       
     }
 }
