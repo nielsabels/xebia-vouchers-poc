aae1786c69d0f3c89c51108f9fc55ebc16cbcf08
Niels Abels <niels.abels@gmail.com>
2020-03-02 12:38:37 +0100

    creation of a voucher

--- Xebia.Vouchers.API/Controllers/VouchersController.cs
+++ Xebia.Vouchers.API/Controllers/VouchersController.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Net;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 using Xebia.Vouchers.API.Dto;
 using Xebia.Vouchers.Domain;
 using Xebia.Vouchers.UseCases;
@@ -11,19 +15,39 @@ namespace Xebia.Vouchers.API.Controllers
     public class VouchersController : ControllerBase
     {
         private readonly CreateVoucherUseCase _createVoucherUseCase;
+        private readonly ILogger _logger;
 
-        public VouchersController(CreateVoucherUseCase createVoucherUseCase)
+        public VouchersController(
+            CreateVoucherUseCase createVoucherUseCase, 
+            ILogger logger)
         {
             _createVoucherUseCase = createVoucherUseCase;
+            _logger = logger;
         }
         
+        /// <summary>
+        /// Create a new voucher
+        /// </summary>
+        /// <param name="voucherTypeDto">Specify which type of voucher to create</param>
+        /// <returns></returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(NewVoucherDto))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<NewVoucher> Post([FromBody]VoucherTypeDto voucherTypeDto)
         {
             VoucherType voucherType = (VoucherType)((int)voucherTypeDto.VoucherType);
 
-            var newVoucher = _createVoucherUseCase.Create(voucherType);
-            return Ok(newVoucher);
+            try
+            {
+                var newVoucherDto = NewVoucherDto.FromDomain(_createVoucherUseCase.Create(voucherType));
+                return StatusCode((int) HttpStatusCode.Created, newVoucherDto);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, "Unable to create a voucher code.");
+                
+                return StatusCode(500, "Could not create a voucher code.");
+            }
         }
     }
 }
